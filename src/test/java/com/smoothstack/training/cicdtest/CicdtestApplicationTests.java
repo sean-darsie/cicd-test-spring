486f195723ac9eef117a675047412b02219f3ad6
Justin O'Brien <justin.obrien@smoothstack.com>
2020-07-12 15:41:17 -0600

    Initial commit

---
+++ src/test/java/com/smoothstack/training/cicdtest/CicdtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.training.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
