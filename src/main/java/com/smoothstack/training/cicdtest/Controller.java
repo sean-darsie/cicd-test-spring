33a90e39f0a751686adb60ec4f81c0badcd8cc6b
Justin O'Brien <justin.obrien@smoothstack.com>
2020-07-17 12:03:02 -0600

    .

--- src/main/java/com/smoothstack/training/cicdtest/Controller.java
+++ src/main/java/com/smoothstack/training/cicdtest/Controller.java
@@ -10,7 +10,7 @@ public class Controller {
 
 	@RequestMapping("/")
 	public ResponseEntity<String> test() {
-		String result = String.join("\n", "I'm a Spring Boot app.", "I'm a GitHub webhook.", System.getenv("VARIABLE"));
+		String result = String.join("\n", "I'm a Spring Boot app.", "I'm a GitHub webhook. ", System.getenv("VARIABLE"));
 		return new ResponseEntity<String>(result, HttpStatus.OK);
 	}
 
